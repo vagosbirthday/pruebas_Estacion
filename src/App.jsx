1cf8ccce347b09656cf7b4b2a0f9e58ac665e01b
Daniel Aguilar <daniel.aguilar7255@gmail.com>
2025-07-11 21:03:34 -0600

    Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CategoryPage from './pages/CategoryPage'
 import ActivityPage from './pages/ActivityPage'
